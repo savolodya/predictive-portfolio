4714b742b2b307a7e78dee7e3ad76b40073b65b8
savol <savolodya@gmail.com>
2024-10-06 21:11:14 +0200

    added profiles, db connection, liquibase, tests, dependencies

---
+++ src/main/java/com/savolodya/predictiveportfolio/PredictivePortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.savolodya.predictiveportfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictivePortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictivePortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/savolodya/predictiveportfolio/PredictivePortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savolodya.predictiveportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictivePortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
