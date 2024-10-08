970148c67ef4e7cbc2e613c05ccdaa095f0bf650
savol <savolodya@gmail.com>
2024-10-08 17:56:05 +0200

    started login

---
+++ src/main/java/com/savolodya/predictiveportfolio/configuration/SecurityConfiguration.java
@@ -0,0 +1,42 @@
+package com.savolodya.predictiveportfolio.configuration;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(
+                                (request, response, authException) -> response.sendError(
+                                        HttpServletResponse.SC_UNAUTHORIZED,
+                                        authException.getMessage()
+                                )
+                        )
+                )
+//                .addFilterBefore(authFilter())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/error").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.PATCH, "/auth/register").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+}
---
+++ src/main/java/com/savolodya/predictiveportfolio/models/login/LoginAccountForm.java
@@ -0,0 +1,3 @@
+package com.savolodya.predictiveportfolio.models.login;
+
+public record LoginAccountForm(String email, String password) {}
